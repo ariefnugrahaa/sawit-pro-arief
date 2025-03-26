d4d9bad5db04bf2eb8440f4f72f9e0f31b422c69
Arief Nugraha <arief@ideku.id>
2025-03-26 21:29:51 +0700

    Initial commit
    This commit sets up the project structure and necessary files for the Sawit Pro Arief application. Key additions include:
    
    -   Added basic project files such as `.gitignore`, `build.gradle.kts`, `settings.gradle.kts`, `gradlew`, `gradlew.bat`, and `gradle-wrapper.properties` for project configuration and build management.
    -   Included essential resources like `backup_rules.xml`, `data_extraction_rules.xml`, `colors.xml`, `strings.xml`, and `themes.xml` within the `res` directory.
    -   Introduced `Color.kt` and `Type.kt` in the `ui.theme` package for custom color schemes and typography.
    -   Added `Theme.kt` in the `ui.theme` package for managing the application's theme, including dark and light color schemes.
    -   Created launcher icons in different densities (`mipmap-hdpi`, `mipmap-mdpi`, `mipmap-xhdpi`, `mipmap-xxhdpi`, `mipmap-xxxhdpi`) with both standard and round versions.
    - Added empty state and filter empty icons.
    - Created `libs.version.toml` to handle dependencies version.
    - Added `proguard-rules.pro` to handle code obfuscation.
    - Added google service json.
    - Created `ic_launcher_background` and `ic_launcher_foreground` vector drawable.
    - Configure application to use hilt, room, workmanager, compose and firebase.

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.example.sawitproarief"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.sawitproarief"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-Xjvm-default=all"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.ui.tooling)
+
+    implementation(libs.navigation.compose)
+
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.hilt.work)
+    kapt(libs.hilt.compiler)
+
+    implementation(libs.bundles.room)
+    kapt(libs.room.compiler)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.database.ktx)
+
+    implementation(libs.work.runtime.ktx)
+
+    testImplementation(libs.bundles.testing)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.test.manifest)
+
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+}
+
+apply(plugin = "com.google.gms.google-services")
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.1.4" apply false
+    id("com.android.library") version "8.1.4" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
+}
