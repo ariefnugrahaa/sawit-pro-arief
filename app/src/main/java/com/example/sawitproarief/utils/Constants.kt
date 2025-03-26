6778336db8c0f0c63989e485933ee5d73aa14f5b
Arief Nugraha <arief@ideku.id>
2025-03-26 21:19:34 +0700

    feat: Add utility functions for data validation and flow combination
    This commit introduces several utility functions and constants to enhance data validation and streamline the combination of flows. Key changes include:
    
    -   Created `combine.kt` to implement a utility function for combining six flows into one, which provides a convenient way to merge multiple data streams.
    -   Added `Constants.kt` to define reusable constants:
        -   `DATE_FORMAT_PATTERN`: Defines the date format.
        -   `FULL_DATE_TIME_FORMAT`: Defines the full date time format.
        - `FilterDefaults`: Defines default filter values such as empty query and default start and end dates.
    -   Implemented `Validator.kt` which contains functions for:
        -   `isValidLicenseNumber`: Validates vehicle license plate numbers using a regular expression.
        -   `validateInboundWeight`: Checks the validity of inbound weight input.
        -   `validateOutboundWeight`: Checks the validity of outbound weight input relative to inbound weight.

---
+++ app/src/main/java/com/example/sawitproarief/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.sawitproarief.utils
+
+object Constants {
+    const val DATE_FORMAT_PATTERN = "dd MMM yyyy"
+    const val FULL_DATE_TIME_FORMAT = "dd MMM yyyy HH:mm:ss"
+    
+    object FilterDefaults {
+        const val EMPTY_QUERY = ""
+        const val DEFAULT_START_DATE = 0L
+        const val DEFAULT_END_DATE = 0L
+    }
+}
